fdebad18e9734e5122ebde39a186208a0d555772
Paul Beattie <Paul@MauleyMedia.co.uk>
2017-08-06 19:04:43 +0100

    Initial Commit

---
+++ MMLUIKitExtension/MMLUIKitExtension.h
@@ -0,0 +1,19 @@
+//
+//  MMLUIKitExtension.h
+//  MMLUIKitExtension
+//
+//  Created by Paul Beattie on 06/08/2017.
+//  Copyright © 2017 Paul Beattie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MMLUIKitExtension.
+FOUNDATION_EXPORT double MMLUIKitExtensionVersionNumber;
+
+//! Project version string for MMLUIKitExtension.
+FOUNDATION_EXPORT const unsigned char MMLUIKitExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMLUIKitExtension/PublicHeader.h>
+
+
